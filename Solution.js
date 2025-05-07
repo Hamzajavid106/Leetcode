fa467236ef50616ee6a0c80363f2ea08d2dfde6a
Hamza Javid <Hamzajavaid614@gmail.com>
2025-05-07 22:15:55 +0500

    Add repo

---
+++ Solution.js
@@ -0,0 +1,45 @@
+// let num = [4,5,6] 
+// let target = 11;
+// // for (let i = 0;i<num.length;i++) {
+//     for (let j = i + 1; j < num.length; j++) {
+                
+//         if (num[i] + num[j] === target) {
+//             console.log(i, j);           
+            
+//         }
+//     }
+// }
+
+
+
+// function twoSum(num,target) {
+//                 for (let i = 0;i<num.length;i++) {
+//                                 for (let j = i + 1; j < num.length; j++) {
+                                            
+//                                     if (num[i] + num[j] === target) {
+//                                 //         console.log(i, j);  
+//                                    return [i,j]         
+                                        
+//                                     }
+//                                 }
+//                             }        
+                
+// }
+// twoSum([1,2,3,4,5,6,7,8,9],9)
+
+// console.log(twoSum[i,j]);
+
+
+
+let l1=[2,3,4,5]
+let l2=[6,7,8,9]
+let sum=(l1,l2)=>{
+   for (let i = 0; i < l1.length; i++) {
+    for (let j = 0; j < l2.length; j++) {
+        console.log([i+j]);
+        
+        
+    }
+    
+   }
+}
